5416cced50ef1a17e411f1f03d8fb521b22193d3
Gustavo Ramalho <gustavo.ramalho@ftssjcd05.fotosensores.local>
2018-04-06 14:16:00 -0300

    Arrumado

--- public/controller/BookController.js
+++ public/controller/BookController.js
@@ -4,38 +4,22 @@ angular.module('app').controller('BookController',
 
     console.log("Entrei BookController");
 
-
-
-    // $scope.books = [{'id':'1','title':'ola','price':'12','description':'ola mundo'}]
-
     $scope.book = {};
 
 
     $scope.salva = function () {
-        // console.log("Foi : " + $scope.book);
-            //Como 'contato' é um objeto retornado de $resource é adicinado funções adicionais ao nosso objeto sem sabermos
-            // A função $save gera por debaixo dos panos uma requisição do tipo POST que envia para http://localhost/contatos
+
         console.log("Tentando : "+$scope.book);
+
         $http.post('http://localhost:8080/books',$scope.book.toJSON()).then(function () {
-            console.log("Tentando pegar!");
-            // console.log("Achei : "+JSON.valueOf(book));
-        $http.post('http://localhost:8080/books',$scope.book.toJSON()).then(function () {
-            // console.log("Tentando pegar!");
-            // console.log("Achei : "+$scope.book);
-            // console.log("Book : "+JSON.stringify($scope.book));
             $scope.mensagem = { texto: 'Alterado com sucesso' };
-            // limpa o formulário
-            // $scope.dados = new DadosEmpresa();
 
         }).catch(function (err) {
             console.log(err);
             $scope.mensagem = { texto: 'Não foi possível Alterar' };
         });
-
-            console.log('Erro');
-            $scope.mensagem = { texto: 'Não foi possível Alterar' };
-        });
         };
+
 
         $scope.pega = function (){
 
@@ -49,16 +33,7 @@ angular.module('app').controller('BookController',
             });
         }
 
-        // Book.query(function(book) {
-        //     $scope.books= book;
-        // });
-
-
-
         books();
-
-
-
 
         if($routeParams.bookId){
             Book.get({id: $routeParams.bookId},
@@ -98,13 +73,7 @@ angular.module('app').controller('BookController',
 
             console.log($scope.book);
             $http.put('http://localhost:8080/books',$scope.book.toJSON()).then(function () {
-                    // console.log("Tentando pegar!");
-                    // console.log("Achei : "+JSON.valueOf(book));
-                    // console.log("Book : "+JSON.stringify($scope.book));
                     $scope.mensagem = { texto: 'Alterado com sucesso' };
-                    // limpa o formulário
-                    // $scope.dados = new DadosEmpresa();
-
                 }).catch(function (erro) {
                         // console.log(err);
                         $scope.mensagem = { texto: 'Não foi possível Alterar' };
@@ -112,32 +81,6 @@ angular.module('app').controller('BookController',
 
 
         };
-
-        $scope.teste = function(book){
-            console.log("clicado");
-        }
-
-        $scope.remove = function(book){
-            console.log(book);
-            $http({
-                method: 'DELETE',
-                url: 'http://localhost:8080/books',
-                params : {id:book.id},
-                headers: {
-                    'Content-type': 'application/json;charset=utf-8'
-                }
-            })
-                .then(function(response) {
-                    console.log(response.data);
-                    console.log("Foi!");
-                }, function(rejection) {
-                    console.log(rejection.data);
-                    console.log("Não foi!");
-                });
-
-
-
-        }
 
 
 
